7ce8bfd76f0027ae5e7e3690f719cf98d13b7313
guyuri <yulrhymz@gmail.com>
2022-03-21 20:43:06 +0900

    [#1] Navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+function Tab1Screen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>스크린1</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Tab2Screen() {
+  return (
+    <View>
+      <Text>스크린2</Text>
+    </View>
+  );
+}
+
+const BottomTab = createBottomTabNavigator();
+function BottomTabNavigator() {
+  return (
+    <BottomTab.Navigator initialRouteName="Tab1">
+      <BottomTab.Screen name="Tab1" component={Tab1Screen} />
+      <BottomTab.Screen
+        name="Tab2"
+        component={Tab2Screen}
+        options={{
+          title: 'Title2',
+          tabBarLabel: 'TabBarLabel2',
+          headerTitleAlign: 'center',
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Root"
+          component={BottomTabNavigator}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
