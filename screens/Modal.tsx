36f2777396b3762ec7a71b8ef5049bcd801fc6bd
guyuri <yulrhymz@gmail.com>
2022-03-28 20:11:00 +0900

    [#1] Modal 완성

--- screens/Modal.tsx
+++ screens/Modal.tsx
@@ -3,7 +3,7 @@ import { View, Button } from 'react-native';
 export default function Modal1Screen({ navigation }: any) {
   return (
     <View>
-      <Button title="Close Modal1" onPress={() => navigation.goBack} />
+      <Button title="Close Modal1" onPress={() => navigation.goBack()} />
     </View>
   );
 }
